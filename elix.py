c4e7a89f867f8c961401c5f9093ac38823a2ceb5
Pritam Singh Shaktawat <shaktawatpritam@gmail.com>
2022-12-27 21:19:53 +0530

    Created new tasks to perform
    Added new functionalities :
    1) Google search
    2) Play music from local directory
    3) To speak the time
    4) To open vs_code
    5) To exit the Elix

--- elix.py
+++ elix.py
@@ -3,7 +3,8 @@ import speech_recognition as sr
 import datetime
 import wikipedia
 import webbrowser
-
+import os
+import pywhatkit
 
 engine=pyttsx3.init('sapi5')
 voices=engine.getProperty('voices')
@@ -16,8 +17,6 @@ def speak(audio):
     engine.say(audio)
     engine.runAndWait()
     
-    
-    
 def greeting():
     hour=int(datetime.datetime.now().hour)
     if hour>=0 and hour<12:
@@ -29,8 +28,8 @@ def greeting():
     
     speak("Hope you are good. My name is Elix, I am a voice assistant developed by Pritam Singh. Please tell me How may I help you ?")
     
-
-
+    
+    
 def inputcommand(): #It will take input or listen from the user's microphone and return string output
     
     r=sr.Recognizer()
@@ -53,7 +52,6 @@ def inputcommand(): #It will take input or listen from the user's microphone and
         
 if __name__=="__main__":
     greeting()
-    
     while True:
         query = inputcommand().lower()  
     
@@ -67,10 +65,44 @@ if __name__=="__main__":
             
         elif 'open google' in query:
             webbrowser.open("google.com")
+        
+        elif 'search google' or 'search on google' in query :
+            speak("What to search ?")
+            s_query=inputcommand().lower()
+            s_query = s_query.replace("search google", "")
             
+            try :
+                pywhatkit.search(s_query)
+                result=wikipedia.summary(s_query,2)
+                speak(result)
+            except Exception as e:
+                print("Sorry, I am not able to search this.")
+             
         elif 'open youtube' in query:
             webbrowser.open("youtube.com")
         
         elif 'open stackoverflow' in query:
             webbrowser.open("stackoverflow.com")
-    
+            
+        elif 'play music' in query:
+            music_dir = 'C:\\Users\\welcome\\Music'
+            songs = os.listdir(music_dir)
+            os.startfile(os.path.join(music_dir,songs[0]))
+            
+        elif 'the time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")
+            speak(f"The time is {strTime}\n")
+            
+        elif 'open code' in query:
+            codePath = 'C:\\Users\\welcome\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe'
+            os.startfile(codePath)
+        
+        elif 'quit' in query:
+            speak("Are you sure you want to exit ?")
+            exit_query = inputcommand().lower()
+            if 'yes' in exit_query:
+                speak("Thank you. Have a nice day.")
+                exit()
+            else:
+                continue
+            
